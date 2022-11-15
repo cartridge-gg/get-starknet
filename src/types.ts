a4c1edd3a4096c28fb2a3174ff87895931ee4f8f
Tarrence van As <tarrence@vanas.family>
2022-11-15 10:46:27 -0500

    feat: add cartridge support

--- src/types.ts
+++ src/types.ts
@@ -170,7 +170,7 @@ export type WalletProvider = {
     id: string;
     name: string;
     icon: string;
-    downloads:
+    downloads?:
         | { chrome?: `https://chrome.google.com/webstore/detail/${string}` }
         | { firefox?: `https://addons.mozilla.org/en-US/firefox/addon/${string}` }
         | { edge?: `https://microsoftedge.microsoft.com/addons/detail/${string}` };
